b2d12fc1a7264049b0a6fc686d337b53427a4c98
Marcelo Sena <marcelomarcos2@gmail.com>
2024-07-02 14:16:31 -0300

    [Feature]: Criação do client e inserção do feing

---
+++ src/main/java/com/sena/hexagonal/adapters/out/client/FindAddressByZipCodeClient.java
@@ -0,0 +1,13 @@
+package com.sena.hexagonal.adapters.out.client;
+
+import com.sena.hexagonal.adapters.out.client.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient (name = "FindAddressByZipCodeClient", url = "${sena.client.address.url}")
+public interface FindAddressByZipCodeClient {
+
+    @GetMapping("/{zipCode}")
+    AddressResponse find (@PathVariable ("zipCode") String zipCode);
+}
