9127a38af6eb73788278a4a9ca9dd20c78e39a63
Marcelo Sena <marcelomarcos2@gmail.com>
2024-07-12 22:53:17 -0300

    [Feature]: Criação do controller, request e inputport

--- src/main/java/com/sena/hexagonal/application/core/usecase/InsertCustomerUseCase.java
+++ src/main/java/com/sena/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -1,10 +1,11 @@
 package com.sena.hexagonal.application.core.usecase;
 
 import com.sena.hexagonal.application.core.domain.Customer;
+import com.sena.hexagonal.application.ports.in.InsertCustomerInputPort;
 import com.sena.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
 import com.sena.hexagonal.application.ports.out.InsertCustomerOutputPort;
 
-public class InsertCustomerUseCase {
+public class InsertCustomerUseCase implements InsertCustomerInputPort {
 
     private final FindAddressByZipCodeOutputPort findAddressByZipCOdeOutputPort;
     private final InsertCustomerOutputPort insertCustomerOutputPort;
@@ -15,6 +16,7 @@ public class InsertCustomerUseCase {
         this.insertCustomerOutputPort = insertCustomerOutputPort;
     }
 
+    @Override
     public void insert(Customer customer, String zipCode){
         var address = findAddressByZipCOdeOutputPort.find(zipCode);
         customer.setAddress(address);
